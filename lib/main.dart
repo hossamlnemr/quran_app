7a84962bfc0bd470d25e7e4023e666bc24958d59
Hossam eldeen Hassan <95591772+hossamlnemr@users.noreply.github.com>
2024-02-25 15:32:12 +0200

    simple quran app

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: unused_import
+
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ytquran/constant.dart';
+import 'index.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    WidgetsBinding
+        .instance
+        .addPostFrameCallback(
+
+            (_) async{
+      await readJson();
+      await getSettings();
+    }
+
+
+
+    );
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quran',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const IndexPage(),
+    );
+  }
+}
